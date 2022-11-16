ba7dc6cd40317a4a8096c55659f0ddb2687b0706
Elyas <ebenyssad@gmail.com>
2022-11-16 12:21:52 -0500

    création de base de la fenetre

--- GameBookScript.py
+++ GameBookScript.py
@@ -1,7 +1,19 @@
 import sys
+import mysql.connector  
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="Exercice11_LDVELH"
+)
+mycursor = mydb.cursor()
+mycursor.execute("SELECT texte FROM chapitre WHERE id = 1")
+requete = mycursor.fetchall()
+
 from PyQt5.QtWidgets import QApplication, QMainWindow
 # Importer la classe Ui_MainWindow du fichier MainWindow.py
-from MonGameBook import Ui_MainWindow
+from MonGameBookV2 import Ui_MainWindow
 
 # En paramêtre de la classe MainWindow on va hériter des fonctionnalités
 # de QMainWindow et de notre interface Ui_MainWindow
@@ -15,13 +27,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     # On défini la fonction qu'on avait déclaré pour le clique sur le bouton
     def continuer(self):
-        cocher = True
-        if self.choix1.isChecked == cocher:
-            self.textChapitre.setText('choix1')
-        if self.choix2:
-            self.textChapitre.setText('choix2')
-        if self.choix3:
-            self.textChapitre.setText('choix3')
+        
+        if self.choix1.isChecked():
+            self.textChapitre.setText(requete[0][0])
+        if self.choix2.isChecked():
+           self.textChapitre.setText('choix2')
+        if self.choix3.isChecked():
+           self.textChapitre.setText('choix3')
 
         
 
